7ca24589c06a9453750c52190acb5b097e4bfd39
TVHieu99 <hieutrantran102@gmail.com>
2023-09-26 18:26:15 +0700

    them home

---
+++ home.js
@@ -0,0 +1,30 @@
+function updateClock() {
+    var clock = document.getElementById('homeclock');
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var day = currentTime.getDate();
+    var month = currentTime.getMonth() + 1; // Tháng bắt đầu từ 0, nên cần cộng 1
+    
+
+    hours = formatTime(hours);
+    minutes = formatTime(minutes);
+    seconds = formatTime(seconds);
+
+    var dateString = day + '/' + month;
+    var timeString = hours + ':' + minutes ;
+
+    clock.innerHTML =  timeString ;
+
+    setTimeout(updateClock, 1000); // Cập nhật đồng hồ sau mỗi giây
+}
+
+function formatTime(time) {
+    if (time < 10) {
+        time = '0' + time;
+    }
+    return time;
+}
+
+updateClock(); // Bắt đầu cập nhật đồng hồ
